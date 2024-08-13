5798a5e22f21a63687c7e0103515bef825ca7ddf
Charlie Syvertsen <65796789+sortofasian@users.noreply.github.com>
2024-08-13 14:24:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-  print("{Main branch}")
+  print("{Main + Rebased branch}")
 
 # This is from the main branch
 # This is another comment from the rebase-merge branch. To merge it, it should only require a rebase with no conflicts
